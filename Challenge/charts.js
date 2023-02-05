6bad9396e820a3afc79162b6aa88dd0a634c1bb2
motoroid <motoroid24@gmail.com>
2023-02-05 12:14:06 -0700

    challege work

--- Challenge/charts.js
+++ Challenge/charts.js
@@ -58,51 +58,99 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   // Deliverable 1: 2. Use d3.json to load the samples.json file 
   d3.json("samples.json").then((data) => {
-    console.log(data);
+    console.log(data);})};
 
     // Deliverable 1: 3. Create a variable that holds the samples array. 
-
+    var sampleArray=[] 
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
+    var desiredSample = sampleArray.filter(function(){
+      return ;
+  });
+    console.log(desiredSample);
 
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
-
+    var metaSample = result.filter(function(){
+      return ;
+  });
+    console.log(metaSample);
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
-
+    var firstSample=[];
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
-
+    var firstMeta = [];
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otu = []; 
     // Deliverable 3: 3. Create a variable that holds the washing frequency.
+    var washingFreq = ["wfreq"];
+
+//     // Deliverable 1: 7. Create the yticks for the bar chart.
+//     // Hint: Get the the top 10 otu_ids and map them in descending order 
+//     // so the otu_ids with the most bacteria are last. 
+
+    var sortedOtu = otu.sort((a,b) => a - b);
+    console.log(sortedOtu);
 
 
-    // Deliverable 1: 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order 
-    // so the otu_ids with the most bacteria are last. 
     var yticks = 
 
     // Deliverable 1: 8. Create the trace for the bar chart. 
-    var barData = [
+    var barTrace = [{
+    x: [],
+    y: [],
+    type: "bar"
+}];
+    var barData = [barTrace];
 
-    ];
 
     // Deliverable 1: 9. Create the layout for the bar chart. 
     var barLayout = {
+    title: "Bellybutton",
+    xaxis: {title: ""},
+    yaxis: {title: ""}
+};
 
-    };
+  // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("plotArea", barData, barlayout);
 
-    // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
 
     // Deliverable 2: 1. Create the trace for the bubble chart.
-
+    var bubbleTrace = {
+      x: [],
+      y: [],
+      mode: 'markers',
+      marker: {
+        size: [40, 60, 80, 100]
+      }
+    };
+    
+    var bubbleData = [bubbleTrace];
+      
     // Deliverable 2: 2. Create the layout for the bubble chart.
-
+    var bubbleLayout = {
+      title: 'Bellybutton',
+      showlegend: false,
+      height: 600,
+      width: 600
+    };
     // Deliverable 2: 3. Use Plotly to plot the data with the layout.
-    
+    Plotly.newPlot('myDiv', bubbleData, bubbleLayout); 
+
+
     // Deliverable 3: 4. Create the trace for the gauge chart.
-    
+  var guageData = [
+	{
+		domain: { x: [], y: [] },
+		value: ,
+		title: { text: "Bellybutton" },
+		type: "",
+		mode: ""
+	}
+];
+
+
+
     // Deliverable 3: 5. Create the layout for the gauge chart.
-
+    var guargeLayout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
-
+    Plotly.newPlot('myDiv', guageData, guageLayout);  
   });
 }
